9fb7cf17438801bb7603e27613c182bab3bc8d52
csxet1 <hmama5810pes@gmail.com>
2026-09-16 02:56:54 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,16 +1,26 @@
-const CACHE_NAME = 'csxet-pwa-v1';
-const APP_SHELL = ['/', '/index.html', '/manifest.json', '/icons/icon-192.png', '/icons/icon-512.png', '/icons/apple-touch-icon.png'];
+const CACHE_NAME = 'csxet-pwa-v2';
+const BASE = new URL('./', self.registration.scope);
+const APP_SHELL = [
+  new URL('./', BASE).href,
+  new URL('index.html', BASE).href,
+  new URL('manifest.json', BASE).href,
+  new URL('icons/icon-192.png', BASE).href,
+  new URL('icons/icon-512.png', BASE).href,
+  new URL('icons/apple-touch-icon.png', BASE).href
+];
 
 self.addEventListener('install', event => {
-  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL)));
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL))
+  );
   self.skipWaiting();
 });
 
 self.addEventListener('activate', event => {
   event.waitUntil(
-    caches.keys().then(keys => Promise.all(
-      keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
-    ))
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
   );
   self.clients.claim();
 });
